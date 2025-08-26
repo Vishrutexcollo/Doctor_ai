88c4c0aec0ca80f0b1040003ab377010c41199bc
Vishrutexcollo <vishrutr@excollo.com>
2025-08-26 16:20:50 +0530

    patient

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ import os
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# MONGODB_URI = os.getenv("MONGODB_URI")
+# MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
-DB_NAME = "doctorai"
-COLLECTION_NAME = "clinicAi"
+# DB_NAME = "doctorai"
+# COLLECTION_NAME = "clinicAi"
--- app/db.py
+++ app/db.py
@@ -7,8 +7,10 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "doctorai")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient("mongodb+srv://vishrutrela:Vishrut123@cluster0.idwtat0.mongodb.net/doctorai")
 db = client["doctorai"]
+
+print(client)
 
 def get_database():
     return db
